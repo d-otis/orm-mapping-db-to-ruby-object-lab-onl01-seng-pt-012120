41070ed2bcb329bcb7bdf9cfd30d9ddac3556dbe
Dan Foley <dan@dan-foley.com>
2020-03-21 18:43:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,8 +23,9 @@ class Student
       LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, name)
-    binding.pry
+    DB[:conn].execute(sql, name).collect do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
