f2c83272ef23db38467a17567cb5ee3292c387c3
Dan Foley <dan@dan-foley.com>
2020-03-21 18:56:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,13 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-      
+      SELECT * 
+      FROM students
+      WHERE grade < 12
     SQL
+    DB[:conn].execute(sql).map do |row|
+      
+    end
   end
 
   def self.find_by_name(name)
