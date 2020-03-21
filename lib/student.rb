919ec5d7a7bdb01c88387893780d0619d2394e1e
Dan Foley <dan@dan-foley.com>
2020-03-21 19:02:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,9 +71,11 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(num)
     sql = <<-SQL
-      
+      SELECT * 
+      FROM students
+      WHERE grade = ?
     SQL
   end
 
