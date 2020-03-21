c2d033c90e14b1780cd8713259c6134ef1d8acee
Dan Foley <dan@dan-foley.com>
2020-03-21 18:52:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,16 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+    binding.pry
+    DB[:conn].execute(sql)
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
